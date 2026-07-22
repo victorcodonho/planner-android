ac002aa5c918799289be09a5269eb613c98dc9a3
Victor Codonho <victor_codonho@outlook.com>
2026-07-21 22:41:57 -0300

    Initial commit

---
+++ app/src/main/java/com/victorcodonho/planner/ui/UserRegistrationFragment.kt
@@ -0,0 +1,36 @@
+package com.victorcodonho.planner.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.victorcodonho.planner.R
+import com.victorcodonho.planner.databinding.FragmentUserRegistrationBinding
+
+class UserRegistrationFragment : Fragment() {
+
+    private var _binding: FragmentUserRegistrationBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentUserRegistrationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            // TODO: Implement user registration logic here
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
